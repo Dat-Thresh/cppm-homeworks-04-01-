2dd3b5cf1d61c35c6f1f942476fbb1c0dc010bba
Maxim Zhvakin <faulpelz1993@gmail.com>
2023-08-30 20:50:52 +0300

    Initial commit

---
+++ cppm-homeworks_04_01/cppm-homeworks_04_01.cpp
@@ -0,0 +1,92 @@
+﻿// cppm-homeworks_04_01.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+class Adress {
+private:
+    std::string city;
+    std::string street;
+    std::string homeNumb;
+    std::string apartment;
+public:
+    //cеттеры
+    void set_city(std::string city) { //устанавливает город в экземпляр класса
+        this->city = city;
+    }
+    void set_street(std::string street) { //устанавливает улицу
+        this->street = street;
+    }
+    void set_homeNumb(std::string homeNumb) { //устанавливает номер дома
+        this->homeNumb = homeNumb;
+    }
+    void set_apartment(std::string apartment) { //устанавливает номер квартиры
+        this->apartment = apartment;
+    }
+    //геттеры
+    std::string get_city() { return city; }
+    std::string get_street() { return street; }
+    std::string get_hoomeNumb() { return homeNumb; }
+    std::string get_apartment() { return apartment; }
+
+    
+};
+
+//возвращает считанный из файла элемент
+std::string return_from_reader(std::ifstream& reader) {
+    std::string buff;
+    reader >> buff;
+    return buff;
+
+}
+//склеивает строку из полей одного элемента массива классов (адресов), чтобы не лепить это в основном коде
+std::string make_Adress_in_one_string(Adress& arr_adress) {
+    std::string buffer;
+    buffer = arr_adress.get_city() + ", " + arr_adress.get_street() + ", " + arr_adress.get_hoomeNumb() + ", " + arr_adress.get_apartment() + "\n";
+    return buffer;
+}
+
+
+int main()
+{
+    std::ifstream reader("in.txt");
+    if (!reader.is_open()) {
+        std::cout << "Unable to open input file!" << std::endl;
+        return 1;
+    }
+    int N; //для записи количества адресов
+    reader >> N;
+    Adress* arr_adress = new Adress[N];
+    for (int i = 0; i < N; i++) {
+        arr_adress[i].set_city(return_from_reader(reader));
+        arr_adress[i].set_street(return_from_reader(reader));
+        arr_adress[i].set_homeNumb(return_from_reader(reader));
+        arr_adress[i].set_apartment(return_from_reader(reader));
+    }
+    reader.close();
+    std::ofstream writer("out.txt");
+    if (!writer.is_open()) {
+        std::cout << "Unable to open output file!" << std::endl;
+        return 2;
+    }
+    //записываем количество адресов
+    writer << N << std::endl;
+    for (int i = 0; i < N; i++) {
+        writer << make_Adress_in_one_string(arr_adress[i]);
+    }
+    writer.close();
+    delete[] arr_adress;
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
